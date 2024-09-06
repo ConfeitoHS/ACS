04154e570769b633b1e4aedc247e6e3e7f41daea
Hosung Lee <43498096+ConfeitoHS@users.noreply.github.com>
2024-09-06 21:40:59 +0900

    Update arcaea.js

--- arcaea.js
+++ arcaea.js
@@ -99,31 +99,33 @@
 
     //for (let i = 0; i < alldiff; i++) { scoredata.push([]); }
     if (window.confirm("본 스크립트는 컨설턴트 시트 " + sheetVer + "에서만 작동합니다. 사용하고 계신 시트의 버전이 이와 맞지 않는 경우, 시트를 미리 업데이트 해 주세요.\n\n" + totaldiff.map((x, i) => (diffnames[i] + ' ' + x)).join(' + ') + ' = ' + totalscores + '개' + "\n\n플레이한 채보가 많으면 시간이 오래 걸릴 수 있습니다. 확인을 누르면 Arcaea Online으로부터 스코어 데이터를 가져옵니다.")) {
-        const top = document.querySelector("section.player-site-content")
-        const toolBg = document.createElement("div");
 
-        toolBg.style.position = "absolute";
-        toolBg.style.top = "0";
-        toolBg.style.zIndex = "10000";
-        toolBg.style.width = "100%";
-        toolBg.style.height = "100%";
-        toolBg.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        const top = document.querySelector("section.player-site-content");
 
-        const infoArea = document.createElement("div");
-        const infoArea2 = document.createElement("div");
+        const infoBG = document.createElement("div");
+        const hexaFore = document.createElement("div");
+        const hexaBack = document.createElement("div");
 
-        infoArea.className = 'myhexagon';
-        infoArea.style.backgroundColor = "#f8f8f9";
-        infoArea.style.zIndex = 4;
-        infoArea.style.marginTop = "-77px";
-        infoArea.style.top = "32.8vh";
+        const infoText = document.createElement("span");
+        const progText = document.createElement("span");
+        const finalText = document.createElement("span");
 
-        infoArea2.className = 'myhexagon';
-        infoArea2.style.top = "33.8vh";
-        infoArea2.style.marginTop = "-77px";
-        infoArea2.style.zIndex = 2;
-        infoArea2.style.backgroundColor = "#d8d8d9";
+        hexaFore.appendChild(infoText);
+        hexaFore.appendChild(progText);
+        hexaFore.appendChild(finalText);
+        infoBG.appendChild(hexaFore);
+        infoBG.appendChild(hexaBack);
+        top.appendChild(infoBG);
+
         var styleElem = document.head.appendChild(document.createElement("style"));
+
+        infoBG.style.position = "absolute";
+        infoBG.style.top = "0";
+        infoBG.style.zIndex = "10000";
+        infoBG.style.width = "100%";
+        infoBG.style.height = "100%";
+        infoBG.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+
 
         styleElem.innerHTML = `.myhexagon{aspect-ratio:3/1;
             position:absolute;
@@ -137,26 +139,36 @@
             color:#000;
             clip-path:polygon(16.6% 0%,83.4% 0%,100% 50%,83.4% 100%,16.6% 100%,0% 50%)}`;
 
-        const infoSpan = document.createElement("span");
-        const numberSpan = document.createElement("span");
-        const lastPara = document.createElement("span");
-        infoSpan.textContent = "데이터 수집중";
-        infoSpan.style.fontSize = '1.5rem';
-        numberSpan.style.fontSize = '1rem';
-        lastPara.style.fontSize = '0.9rem';
-        lastPara.style.width = '75%';
-        lastPara.style.textAlign = 'center';
 
-        infoArea.appendChild(infoSpan);
-        infoArea.appendChild(numberSpan);
-        infoArea.appendChild(lastPara);
-        toolBg.appendChild(infoArea);
-        toolBg.appendChild(infoArea2);
-        top.appendChild(toolBg);
+        //let _mtop = `${-Math.round(Math.min(window.innerWidth, 600) * 0.256) / 2}px`;
+        hexaFore.className = 'myhexagon';
+        hexaFore.style.backgroundColor = "#f8f8f9";
+        hexaFore.style.zIndex = 4;
+        hexaFore.style.marginTop = "max(-12.8vw, -77px)";
+        hexaFore.style.top = "32.8vh";
+
+        hexaBack.className = 'myhexagon';
+        hexaBack.style.top = "33.8vh";
+        hexaBack.style.marginTop = "max(-12.8vw, -77px)";
+        hexaBack.style.zIndex = 2;
+        hexaBack.style.backgroundColor = "#d8d8d9";
+
+        infoText.textContent = "데이터 수집중";
+        infoText.style.fontSize = 'min(24px,4vw)';
+        infoText.style.fontWeight = 'bold';
+        progText.style.fontSize = 'min(18px,3vw)';
+        progText.style.textAlign = 'center';
+        finalText.style.fontSize = 'min(12px,2vw)';
+        finalText.style.width = '80%';
+        finalText.style.textAlign = 'center';
+
+
 
         let currentpage = 0;
 
         try {
+            let lasttime = Date.now();
+            let meaninterval = 0;
             const scoreList = [];
             for (let k = 0; k < alldiff; k++) {
                 for (let p = 1; p <= pagesall[k]; p++) {
@@ -167,8 +179,14 @@
                     scoreList.push(...(scoredata["value"]["scores"].map(basicMapper)));
 
                     currentpage++;
-                    numberSpan.textContent = `${currentpage} / ${totalpages}`;
-                    await _sleep(getRandomInt(300, 600));
+                    await _sleep(getRandomInt(600, 1100));
+                    let interval = Date.now() - lasttime;
+                    lasttime = Date.now();
+                    meaninterval = meaninterval + (interval - meaninterval) / currentpage;
+                    progText.innerHTML = `${currentpage} / ${totalpages}<br>`;
+                    if (currentpage > 5) {
+                        progText.innerHTML += `${(Math.round((totalpages - currentpage) * meaninterval / 1000)).toString()}초 남음`;
+                    }
                 }
             }
             console.log('scores collected, converting start');
@@ -195,18 +213,19 @@
                     return
                 }
                 but.innerText = "복사됨!"
-                but.style.backgroundColor = "lightskyblue";
-                but.style.color = "midnightblue"
+                but.style.backgroundColor = "#a3edff";
+                but.style.color = "#1c1c1c"
             }
+
             copybutton.id = 'lastcopybutton';
-            copybutton.style.cssText = "width: 30%;clip-path: polygon(16.6% 0%,83.4% 0%,100% 50%,83.4% 100%,16.6% 100%,0% 50%);aspect-ratio: 3/1;z-index: 10001;top: 50vh;left: 35%;position: absolute;background-color: lavender;border-width: 3px 0px;box-shadow: 5px 5px black;font-size: 1.2rem; font-weight: bold;";
+            copybutton.style.cssText = "width: 30%;clip-path: polygon(16.6% 0%,83.4% 0%,100% 50%,83.4% 100%,16.6% 100%,0% 50%);aspect-ratio: 3/1;z-index: 10001;top: 50vh;left: 35%;position: absolute;background-color: lavender;box-shadow: 5px 5px black;font-size: min(18px,3vw); font-weight: bold;";
             copybutton.textContent = "복사하기";
             copybutton.onclick = buttonclick;
-            toolBg.append(copybutton);
+            infoBG.append(copybutton);
 
-            infoSpan.textContent = scoreList.length + '개의 기록이 수집되었습니다.';
-            numberSpan.textContent = '';
-            lastPara.innerHTML = "1. 아래 버튼을 눌러 결과 복사<br>2. 컨설턴트 시트의 필터를 모두 해제<br>3. 업뎃 순으로 오름차순 정렬<br>4. Sayonara Hatsukoi FTR의 점수 기입칸에 붙여넣기";
+            infoText.textContent = scoreList.length + '개의 기록이 수집되었습니다.';
+            progText.textContent = '';
+            finalText.innerHTML = "1. 아래 버튼을 눌러 결과 복사<br>2. 컨설턴트 시트의 필터를 모두 해제<br>3. 업뎃 순으로 오름차순 정렬<br>4. Sayonara Hatsukoi FTR의 점수 기입칸에 붙여넣기";
 
             //window.alert(scoreList.length + '개의 기록이 수집되었습니다.' + "결과가 클립보드에 복사되었습니다. 컨설턴트 시트의 1.필터를 모두 해제하고 2.업뎃 순으로 오름차순 정렬한 후 3.Sayonara Hatsukoi FTR의 점수 기입칸에 붙여넣으세요.");
 
